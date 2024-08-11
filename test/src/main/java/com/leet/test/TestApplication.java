d3fce21703273fc577fe4742bbf3c3670cb0b69b
bucky1005 <null00007@naver.com>
2024-08-11 23:34:06 +0900

    init

---
+++ test/src/main/java/com/leet/test/TestApplication.java
@@ -0,0 +1,13 @@
+package com.leet.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+        // 매일 1-2문제씩 풀기
+}
