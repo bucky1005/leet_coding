64621565e10ce6fbb169b5ccab5d4de5e3ada5fb
bucky1005 <null00007@naver.com>
2024-08-30 19:14:47 +0900

    data structure - stack

--- test/src/main/java/com/leet/test/TestApplication.java
+++ test/src/main/java/com/leet/test/TestApplication.java
@@ -3,16 +3,23 @@ package com.leet.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Stack;
+
 @SpringBootApplication
 public class TestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
 
-        int a, b;
+        Stack<Integer> stack = new Stack<>();//push, pop, peek, empty, seach 지원
+        for (int i = 1; i <= 5; i++) {
+            stack.push(i);
+            System.out.println(stack.peek());
+        } //1, 2, 3, 4, 5 출력
+        stack.pop();
+        System.out.println("Pop()");
+        System.out.println(stack.peek());    //4출력
+        System.out.println(stack.search(3));    //2출력
+        System.out.println(stack.empty());    //false출력
     }
-        // 매일 1-2문제씩 풀기
-        // 8/26 부터 바로 시작
-        // the leet code beginner
-
 }
