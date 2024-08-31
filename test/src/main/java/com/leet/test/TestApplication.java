631b60c6fdb56492a697dfda5eba65f3c502a2ce
bucky1005 <null00007@naver.com>
2024-08-31 23:57:30 +0900

    data structure - queue

--- test/src/main/java/com/leet/test/TestApplication.java
+++ test/src/main/java/com/leet/test/TestApplication.java
@@ -3,6 +3,8 @@ package com.leet.test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 @SpringBootApplication
@@ -11,6 +13,7 @@ public class TestApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
 
+        /* Stack */
         Stack<Integer> stack = new Stack<>();//push, pop, peek, empty, seach 지원
         for (int i = 1; i <= 5; i++) {
             stack.push(i);
@@ -21,5 +24,18 @@ public class TestApplication {
         System.out.println(stack.peek());    //4출력
         System.out.println(stack.search(3));    //2출력
         System.out.println(stack.empty());    //false출력
+
+        /* Queue */
+        Queue<Integer> queue = new LinkedList<Integer>();
+
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(3);
+        queue.offer(4);
+        queue.offer(5);
+
+        while(!queue.isEmpty()) {
+            System.out.println(queue.poll());
+        }
     }
 }
