261d8441a805dd65a14e09a94307cb660c678001
bucky1005 <null00007@naver.com>
2024-09-07 23:45:11 +0900

    data structure - PriorityQueue

---
+++ test/src/main/java/com/leet/dataStructure/PriorityQueue.java
@@ -0,0 +1,65 @@
+package com.leet.dataStructure;
+
+import java.util.Comparator;
+
+public class PriorityQueue<E> implements Queue<E> {
+
+    private final Comparator<? super E> comparator;
+    private static final int DEFAULT_CAPACITY = 10;	// 최소(기본) 용적 크기
+
+    private int size;	// 요소 개수
+    private Object[] array;	// 요소를 담을 배열
+
+    // 생성자 Type 1 (초기 공간 할당 X)
+    public PriorityQueue() {
+        this(null);
+    }
+
+    public PriorityQueue(Comparator<? super E> comparator) {
+        this.array = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+        this.comparator = comparator;
+    }
+
+    // 생성자 Type 2 (초기 공간 할당 O)
+    public PriorityQueue(int capacity) {
+        this(capacity, null);
+    }
+
+    public PriorityQueue(int capacity, Comparator<? super E> comparator) {
+        this.array = new Object[capacity];
+        this.size = 0;
+        this.comparator = comparator;
+    }
+
+
+    // 받은 인덱스의 부모 노드 인덱스를 반환
+    private int getParent(int index) {
+        return index / 2;
+    }
+
+    // 받은 인덱스의 왼쪽 자식 노드 인덱스를 반환
+    private int getLeftChild(int index) {
+        return index * 2;
+    }
+
+    // 받은 인덱스의 오른쪽 자식 노드 인덱스를 반환
+    private int getRightChild(int index) {
+        return index * 2 + 1;
+    }
+
+    @Override
+    public boolean offer(E e) {
+        return false;
+    }
+
+    @Override
+    public E poll() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+}
